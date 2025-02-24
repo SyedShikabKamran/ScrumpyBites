15718ab34236463243264c9c4c58900c1301e7ae
SyedShikabKamran <144217554+SyedShikabKamran@users.noreply.github.com>
2025-02-25 02:54:35 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Backend is running!'));
+module.exports = app;
+
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
